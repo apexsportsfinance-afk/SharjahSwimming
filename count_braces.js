998c15f209461169b18e8c7f2213ad4246002c56
apexsportsfinance-afk <apexsportsfinance@gmail.com>
2026-07-02 13:44:30 +0400

    Delete count_braces.js

--- count_braces.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:\\Users\\Administrator\\OneDrive\\Desktop\\pro\\src\\pages\\admin\\Events.jsx', 'utf8');
-const openBraces = (content.match(/{/g) || []).length;
-const closeBraces = (content.match(/}/g) || []).length;
-console.log(`Open: ${openBraces}, Close: ${closeBraces}`);
